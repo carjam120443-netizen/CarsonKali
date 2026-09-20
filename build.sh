092b565b1411374683cef9c105de39f1fd0faf3b
carjam120443-netizen <carjam120443@gmail.com>
2026-09-20 13:57:18 -0400

    Remove unsupported live-build updates option

--- build.sh
+++ build.sh
@@ -23,8 +23,7 @@ sudo lb config \
   --mirror-bootstrap "http://http.kali.org/kali" \
   --mirror-chroot "http://http.kali.org/kali" \
   --mirror-binary "http://http.kali.org/kali" \
-  --security false \
-  --updates false
+  --security false
 
 # Kali Rolling has a single rolling suite; it does not publish a
 # separate kali-rolling-updates or kali-rolling-security suite.
